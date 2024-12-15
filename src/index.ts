e7155fe174cfbf528c88f75caa1e2bb675635b59
2underscores <jeremy__smith@hotmail.com>
2024-12-15 20:56:56 +1100

    zip file upload working

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
 import { syncSlackWithOpsgenie, type ScheduleToSlackGroupChannel } from "./syncOpsgenieSlack.js";
 
 const RosterSlackMappings: ScheduleToSlackGroupChannel[] = [
-  // Actual Platform squad alerting. Sends message and updates support group
   {
     opsgenieScheduleName: 'Platform_schedule',
-    // slackChannelName: 'squad-platform', // Can be provided, but expensive paginated API call. Use Channel ID instead
-    // slackChannelId: 'C05MD9Y10P9',
-    slackChannelId: 'C084X18A92N', // Sends to #tmp-test-opsgenie-sync,
+    slackChannelId: 'C084X18A92N', // #tmp-test-opsgenie-sync
     slackGroupName: 'Platform Support',
   },
-  // // Example Squad1 only update group membership
-  // {
-  //   opsgenieScheduleName: 'Squad1schedule',
-  //   slackGroupName: 'Squad1 Roster',
-  // },
-  // // Example squad2 only send message to channel
-  // {
-  //   opsgenieScheduleName: 'Squad2 - Operations Schedule',
-  //   slackChannelId: 'C084X18A92N', // Sends to #tmp-test-opsgenie-sync,
-  // },
-  // // Other squad configs below here
-]
+  {
+    opsgenieScheduleName: 'Platform_schedule',
+    slackChannelId: 'C05MD9Y10P9', 
+    slackGroupName: 'Platform Support',
+  },
+];
 
-// Local trigger handler
-if (import.meta.url === new URL(import.meta.url).href) {
-  const results = await syncSlackWithOpsgenie(RosterSlackMappings);
-  console.log({ results });
-  const body = JSON.stringify({ results }); // Make sure works for lambda
-}
+export const handler = async (event: any) => {
+  console.log('Handler started');
+  try {
+    console.log('About to sync');
+    const results = await syncSlackWithOpsgenie(RosterSlackMappings);
+    console.log('Sync completed:', results);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ results })
+    };
+  } catch (error) {
+    console.error('Error in handler:', error);
+    throw error;
+  }
+};
